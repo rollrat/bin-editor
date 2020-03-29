a5561d050d582876c63ae1eb86571c66e242fd05
rollrat <rollrat.cse@gmail.com>
2020-03-29 12:27:52 +0900

    target: Implements function splitter on .text section level

--- linux-recompiler/Program.cs
+++ linux-recompiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿// This source code is a part of Sharp Linux Recompiler
 // Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
 
+using SharpDisasm;
 using System;
 
 namespace linux_recompiler
@@ -9,7 +10,16 @@ namespace linux_recompiler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t = new Target(@"C:\Users\rollrat\source\repos\linux-recompiler\test\1. Hello World\a.out");
+
+            foreach (var func in t.Functions)
+            {
+                Console.WriteLine($"{func.Item1.Name}:");
+                func.Item2.ForEach(x => Console.WriteLine($"{x}"));
+                Console.WriteLine($"");
+            }
+
+            ;
         }
     }
 }
--- linux-recompiler/Target.cs
+++ linux-recompiler/Target.cs
@@ -2,9 +2,12 @@
 // Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
 
 using ELFSharp.ELF;
+using ELFSharp.ELF.Sections;
+using SharpDisasm;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace linux_recompiler
@@ -14,12 +17,83 @@ namespace linux_recompiler
         public byte[] Bytes { get; private set; }
         public IELF ELF { get; private set; }
         FileStream fs;
+        public List<(ISymbolEntry, List<Instruction>)> Functions { get; private set; }
 
         public Target(string program_path)
         {
             Bytes = File.ReadAllBytes(program_path);
-            fs = new FileStream("test/1. Hello World/a.out", FileMode.Open);
+            fs = new FileStream(program_path, FileMode.Open);
             ELF = ELFReader.Load(fs, false);
+            Functions = new List<(ISymbolEntry, List<Instruction>)>();
+
+            var funcs = ((ISymbolTable)ELF.GetSection(".symtab")).Entries.Where(x => x.Type == SymbolType.Function).ToList();
+            var symfuncs = new Dictionary<ulong, ISymbolEntry>();
+            var symextern = new List<ISymbolEntry>();
+            funcs.ForEach(x => {
+                var os = (x as SymbolEntry<ulong>).Value;
+                if (os != 0)
+                    symfuncs.Add(os, x);
+                else
+                    symextern.Add(x);
+                });
+
+            var insts = disasm(ELF.GetSection(".text").GetContents()).ToList();
+            var symbols = symfuncs.ToList();
+            var symbols_index = 0;
+            ulong base_offset = (ELF.GetSection(".text") as Section<ulong>).Offset;
+            symbols.Sort((x,y) => x.Key.CompareTo(y.Key));
+            symbols.Add(new KeyValuePair<ulong, ISymbolEntry>(ulong.MaxValue, null));
+
+            //foreach (var func in funcs)
+            //{
+            //    var se = (func as SymbolEntry<int>);
+            //    Functions.Add(func, disasm(Bytes.Skip(se.Value).Take(se.Size).ToArray()));
+            //}
+
+            var added = new HashSet<ISymbolEntry>();
+
+            for (int i = 0; i < insts.Count; )
+            {
+                if (insts[i].Offset + base_offset == symbols[symbols_index].Key)
+                {
+                    var func = new List<Instruction>();
+                    for (; i < insts.Count && insts[i].Offset + base_offset < symbols[symbols_index + 1].Key; i++)
+                    {
+                        func.Add(insts[i]);
+                    }
+                    for (int j = func.Count - 1; j >= 0; j--)
+                    {
+                        if (func[j].Mnemonic == SharpDisasm.Udis86.ud_mnemonic_code.UD_Inop)
+                            func.RemoveAt(j);
+                        else
+                            break;
+                    }
+                    Functions.Add((symbols[symbols_index].Value, func));
+                    added.Add(symbols[symbols_index].Value);
+                    symbols_index += 1;
+                }
+                else if (insts[i].Offset + base_offset > symbols[symbols_index].Key)
+                {
+                    symbols_index++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            symbols.RemoveAt(symbols.Count - 1);
+            Functions.Add((symbols.Where(x => x.Value.Name == "_init").Single().Value, 
+                disasm(ELF.GetSection(".init").GetContents()).ToList()));
+            added.Add(symbols.Where(x => x.Value.Name == "_init").Single().Value);
+
+            foreach (var ise in symbols)
+            {
+                if (!added.Contains(ise.Value))
+                    Functions.Add((ise.Value, new List<Instruction>()));
+            }
+
+            ;
         }
 
         ~Target()
@@ -27,6 +101,17 @@ namespace linux_recompiler
             fs.Close();
         }
 
+        static List<Instruction> disasm(byte[] bytes)
+        {
+            ArchitectureMode mode = ArchitectureMode.x86_64;
+            var disasm = new Disassembler(
+                bytes,
+                mode, 0, true);
 
+            Disassembler.Translator.IncludeAddress = true;
+            Disassembler.Translator.IncludeBinary = true;
+
+            return disasm.Disassemble().ToList();
+        }
     }
 }
